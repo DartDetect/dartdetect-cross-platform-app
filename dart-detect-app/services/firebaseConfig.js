a3d2947a8018ae2bddba3eefcc680f1515afb911
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2024-11-24 19:55:03 +0000

    added firebase config

---
+++ dart-detect-app/services/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRnWyDIQpeP-Jwr8-HmADkIoORSDKuNss",
+  authDomain: "dartdetect-88d33.firebaseapp.com",
+  projectId: "dartdetect-88d33",
+  storageBucket: "dartdetect-88d33.firebasestorage.app",
+  messagingSenderId: "1090343665682",
+  appId: "1:1090343665682:web:a9b27fe7a19b6ae56b18cb",
+  measurementId: "G-SS1C6QG148"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
