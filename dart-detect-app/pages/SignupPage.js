4d10c60f7ab795b2d27a017cc47fdb20ec866ee9
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2024-11-25 12:09:22 +0000

    Added singup button and navigation link to SignupPage

--- dart-detect-app/pages/SignupPage.js
+++ dart-detect-app/pages/SignupPage.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, useWindowDimensions} from "react-native";
 
-export default function SignupPage() {
+export default function SignupPage({navigation}) {
 
   // State Variables for email and password inputs
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const {width} = useWindowDimensions(); // Get device width for responsive design
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Sign Up</Text>
       <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} keyboardType="email-address" autoCapitalize="none"/>
       <TextInput style ={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+      <Button title="Sign Up" />
+      <text style ={styles.link} onPress={()=> navigation.navigate("LoginPage")}>
+        Already have an account? Login Here
+      </text>
+
     </View>
   );
 }
@@ -36,6 +43,13 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginBottom: 15,
-  }
+  },
+
+    link: {
+      color: "blue",
+      marginTop: 10,
+      textAlign: "center",
+    }
+
 
 });
