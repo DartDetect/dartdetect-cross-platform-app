e2e2cb10b83e49abf10a6b3896beec9868f1b5bb
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2024-11-25 11:51:04 +0000

    Added email and password input fields to SignupPage

--- dart-detect-app/pages/SignupPage.js
+++ dart-detect-app/pages/SignupPage.js
@@ -1,14 +1,22 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 export default function SignupPage() {
+
+  // State Variables for email and password inputs
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Sign Up</Text>
+      <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} keyboardType="email-address" autoCapitalize="none"/>
+      <TextInput style ={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
     </View>
   );
 }
 
+//Component Stylesheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,4 +27,15 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
   },
+
+  //  Styling for email and password input fields
+  input: {
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 15,
+  }
+
 });
