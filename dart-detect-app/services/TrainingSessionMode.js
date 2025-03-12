24f2caad07a30ce56565f73299046a422fce39ab
JakubSzkup1 <jakub281@hotmail.com>
2025-03-12 16:23:19 +0000

    Initialise training session state

---
+++ dart-detect-app/services/TrainingSessionMode.js
@@ -0,0 +1,11 @@
+// Training Session Mode logic
+
+// Initialise training session state
+export const InitialiseSession = () => ({
+    currentRound: 1,
+    roundScores: [],
+    currentRoundScore: 0,
+});
+
+
+
