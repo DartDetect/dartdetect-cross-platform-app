49208f356f05492f6ed67c28324fac474cca12a4
JakubSzkup1 <jakub281@hotmail.com>
2025-04-26 14:49:11 +0100

    Rotated  API Key and hid api in .env

--- dart-detect-app/services/firebaseConfig.js
+++ dart-detect-app/services/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRnWyDIQpeP-Jwr8-HmADkIoORSDKuNss",
-  authDomain: "dartdetect-88d33.firebaseapp.com",
-  projectId: "dartdetect-88d33",
-  storageBucket: "dartdetect-88d33.firebasestorage.app",
-  messagingSenderId: "1090343665682",
-  appId: "1:1090343665682:web:a9b27fe7a19b6ae56b18cb",
-  measurementId: "G-SS1C6QG148"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:  process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
